e61dc0adc8dfe714cc1a8ca1bbc974e3b93341fd
João de Menezes Ferreira Júnior <118396440+Joao-de-Menezes@users.noreply.github.com>
2025-05-26 22:43:30 +0000

    Remove: apagado arquivo inútil

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import { App } from './app';
-
-test('Renderiza o título corretamente', () => {
-  render(<App />);
-  expect(screen.getByPlaceholderText('Busque em suas tarefas aqui...')).toBeInTheDocument();
-});
