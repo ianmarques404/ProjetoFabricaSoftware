7e39e2173bcf00b07a149e0c49a517bcb8b21184
João de Menezes Ferreira Júnior <118396440+Joao-de-Menezes@users.noreply.github.com>
2025-05-26 22:41:27 +0000

    Add: instalando Jest para testes na aplicação

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
-import { App } from './App';
+import { App } from './app';
 
 test('Renderiza o título corretamente', () => {
   render(<App />);
